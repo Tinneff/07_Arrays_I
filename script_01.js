de4a494c0576ba6b3a09062fb673c2f5bc169e13
Tinneff <tadeus@unitybox.de>
2023-01-19 12:21:29 +0100

    version 1 ++

--- script_01.js
+++ script_01.js
@@ -3,22 +3,68 @@
 /* Die Satzbau-Maschine | Arrays */
 
 /*** 02. Theorie: Array */
+// let arr;
+// arr = new Array();  // Konstruktor
+// arr = []; // Literal
+// arr = [2,7,11,4];
+// arr = [true,false,true];
+// arr = ["Ich","bin","Max","Mütze"];
+
+// output(arr);
+// output(arr.length); // Anzahl der Elemente
+// output(arr[0]); // Index 0 (1. Pos. im Array)
+// output(arr[3]); // letze Pos. / konkret
+// output(arr[arr.length-1]); // letze Pos. / allgemein
+
+// () --> Parameter,Tests
+// {} --> Codeblöcke
+// [] --> Arrays, Index
+
+
+
+
 
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
-output(getSentence("Ich","bin","Max","Mütze"));
-function getSentence(word1,word2,word3,word4) {
+// output(getSentence("Ich","bin","Max","Mütze"));
+// output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+function getSentence(word1,word2,word3,word4,word5, word6) {
     const GAP = " ";
     const PUNCT = ".";
     let str =   word1 + GAP +
                 word2 + GAP +
                 word3 + GAP +
-                word4 +
+                word4 + GAP +
+                word5 + GAP +
+                word6 +
                 PUNCT;
 
     return str;
 }
+
+
+/*** 01a. Funktionalität mit Array 1  */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n] ::Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP +
+                arr[5] +
+                PUNCT;
+
+    return str;
+}
+
+
 
 
 
