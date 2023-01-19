4b2ebddfebc30d5b94de778edb16f0e646783670
Peter <schmidt.multimedia@gmail.com>
2023-01-19 10:56:52 +0100

    01. Funktionalität mit Einzelparametern / 1. Iteration

--- script_01.js
+++ script_01.js
@@ -7,6 +7,18 @@
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
+output(getSentence("Ich","bin","Max","Mütze"));
+function getSentence(word1,word2,word3,word4) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   word1 + GAP +
+                word2 + GAP +
+                word3 + GAP +
+                word4 +
+                PUNCT;
+
+    return str;
+}
 
 
 
